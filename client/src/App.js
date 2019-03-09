d1eab1aac2541a3e4153cc06179993a30294aa66
Jacob Lamont <cob@jacoblamont.com>
2019-03-09 17:16:37 -0500

    adds react-router-dom into the mix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 import axios from "axios";
+import {BrowserRouter, Route, Link, Redirect} from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
 import UserContext from "./context/UserContext";
+
+const Router = props => (
+	<BrowserRouter>
+		<div>
+			{props.children}
+		</div>
+	</BrowserRouter>
+);
+
+const ProtectedRoute = ({component: Component, ...rest}) => (
+	<Route {...rest} render={props => (
+		false ? (
+			<Component {...props} />
+		) : (
+			<Redirect to="/login" />
+		)
+	)} {...rest} />
+);
 
 class App extends Component {
 
@@ -25,15 +45,17 @@ class App extends Component {
 	const {user} = this.state;
 	const login = this.login;
     return (
-		<UserContext.Provider value={{ login, user }}>
-			{user ? (
-				<div>
-					<h1>Welcome {user.username}</h1>
-				</div>
-			) : (
-				<LoginPage />
-			)}
-		</UserContext.Provider>
+		<Router>
+			<header>
+				<nav>
+					<Link to="/">Home</Link> | <Link to="/login">Login</Link>
+				</nav>
+			</header>
+			<UserContext.Provider value={{ login, user }}>
+				<ProtectedRoute exact path="/" component={HomePage} />
+				<Route exact path="/login" component={LoginPage} />
+			</UserContext.Provider>
+		</Router>
     );
   }
 }
